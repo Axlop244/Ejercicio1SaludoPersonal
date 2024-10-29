4e0e4bd112b58a7e2ab143608169e41a32295d20
Alisson Ximena Lopez Barajas <alisson09.barajas@gmail.com>
2024-10-29 15:11:55 -0500

    Ejercicio1SaludoPersonal

---
+++ SaludoPersonal.java
@@ -0,0 +1,8 @@
+// Usando como ejemplo el archivo HolaMundo, crea un archivo llamado SaludoPersonal 
+//que imprima: Cesar, bienvenido al curso de Programación en Java!!!
+public class SaludoPersonal{
+    public static void main(String[] args){
+        //Imprimir en el terminal, consola 
+        System.out.print("Alisson, bienvenido al curso de Programación en Java!!!"); 
+    }
+}
